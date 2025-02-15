0ca1a6530ad08452a132b5abb2a6989e0737e3d1
SunSundr <art.dizigner@gmail.com>
2025-02-15 18:49:44 +0400

    test: update aws-deploy.test.ts

--- aws-deploy/test/aws-deploy.test.ts
+++ aws-deploy/test/aws-deploy.test.ts
@@ -1,10 +1,10 @@
-import { Template } from 'aws-cdk-lib/assertions';
+import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as cdk from 'aws-cdk-lib';
 import { AwsDeployStack } from '../lib/aws-deploy-stack';
 
-test('S3 Bucket Created', () => {
+test('S3 Bucket and CloudFront Created with OAI', () => {
   const app = new cdk.App();
-  const stack = new AwsDeployStack(app, 'MyTestStack');
+  const stack = new AwsDeployStack(app, 'TestStack');
   const template = Template.fromStack(stack);
 
   template.hasResourceProperties('AWS::S3::Bucket', {
@@ -13,7 +13,37 @@ test('S3 Bucket Created', () => {
 
   template.hasResourceProperties('AWS::CloudFront::Distribution', {
     DistributionConfig: {
+      Origins: Match.arrayWith([
+        Match.objectLike({
+          DomainName: Match.anyValue(),
+          S3OriginConfig: {
+            OriginAccessIdentity: Match.objectLike({
+              'Fn::Join': Match.anyValue(),
+            }),
+          },
+        }),
+      ]),
+    },
+  });
+
+  template.hasResourceProperties('AWS::CloudFront::Distribution', {
+    DistributionConfig: {
       DefaultRootObject: 'index.html',
+    },
+  });
+
+  template.hasResourceProperties('AWS::S3::BucketPolicy', {
+    PolicyDocument: {
+      Statement: Match.arrayWith([
+        Match.objectLike({
+          Effect: 'Allow',
+          Principal: {
+            CanonicalUser: Match.anyValue(),
+          },
+          Action: 's3:GetObject',
+          Resource: Match.anyValue(),
+        }),
+      ]),
     },
   });
 });
