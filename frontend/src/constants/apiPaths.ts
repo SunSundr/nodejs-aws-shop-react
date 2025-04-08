be4025e31f01ad153ad6f00c31d306b397e68e3a
Aleksandr Kovalenko <160144047+SunSundr@users.noreply.github.com>
2025-04-08 04:52:38 +0400

    Task 7 (Authorization, Updating the Frontend Application) (#6)
    * feat: use token for authorization-service
    
    * feat: add cognito authorization

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -12,3 +12,21 @@ const API_PATHS = {
 export default API_PATHS;
 
 export const RESERVED_ID_PREFIX = '7567ec4b-b10c-45c5-9345-fc73c48a80';
+
+export const LOCALSTORAGE_AUTH_TOKEN_KEY = 'authorization_token';
+
+const clientId = '6herb483m1sk3mhg2hui2p6vfc';
+const cognitoDomain = 'https://sunsundr-auth.auth.eu-north-1.amazoncognito.com';
+export const COGNITO_AUTH_CONFIG = {
+  authority: 'https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_az0zQqLAU',
+  client_id: clientId,
+  redirect_uri: `${window.location.origin}/login`,
+  response_type: 'code',
+  scope: 'email openid profile',
+  automaticSilentRenew: true,
+  loadUserInfo: true,
+  // userStore: new WebStorageStateStore({
+  //   store: localStorage,
+  // }),
+};
+export const LOGOUT_URL = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(`${window.location.origin}/logout`)}`;
